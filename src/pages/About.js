8c7ff06ddcfeadc27f5e8911536f4dc189763a2b
rajat <rajat.k@sphinxworldbiz.co>
2023-07-19 18:52:54 +0530

    Pages created, navbar component created, working on article-content page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Home = () => {
+const About = () => {
   return (
     <div className='mb-20'>
-        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>Hello, Welcome 2 MERN Blog</h1>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>About</h1>
         <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde obcaecati blanditiis adipisci reiciendis animi velit dolores atque cupiditate placeat, exercitationem magnam aut delectus incidunt in odit dicta nobis veniam minus vero veritatis soluta mollitia. Consequuntur sunt a totam possimus suscipit iure necessitatibus, inventore dolorum incidunt est porro blanditiis, omnis magni!</p>
         <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde obcaecati blanditiis adipisci reiciendis animi velit dolores atque cupiditate placeat, exercitationem magnam aut delectus incidunt in odit dicta nobis veniam minus vero veritatis soluta mollitia. Consequuntur sunt a totam possimus suscipit iure necessitatibus, inventore dolorum incidunt est porro blanditiis, omnis magni!</p>
         <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ratione sunt modi inventore eius voluptate perferendis sapiente quam adipisci. Quia reiciendis expedita facere voluptatem ullam. Illo corrupti accusantium magnam pariatur odio, libero repudiandae nobis autem hic totam dignissimos debitis molestias tempora cum delectus. Deleniti exercitationem, ratione praesentium, delectus optio dicta iure perferendis quas dignissimos eaque, quam dolore officiis repellat eum numquam error voluptate incidunt excepturi. Illo libero magnam quam mollitia vel pariatur earum quas vitae ullam error cumque corporis saepe commodi accusamus hic, autem, eveniet enim quia? Dignissimos quae officiis eius dolores sequi consequuntur facilis esse perspiciatis, molestiae quas commodi.</p>
@@ -11,4 +11,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default About
